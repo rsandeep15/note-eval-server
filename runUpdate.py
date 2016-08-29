d6172a09ade595597f51f5fbcaa253a12a135db5
Sandeep Raghunandhan <sraghunandhan@shutterfly.com>
2016-08-28 23:26:24 -0700

    Adding handler for post request
    Obtains file name and userUID from the request to be passed into the
    pricePopulating python function

---
+++ runUpdate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+import cgitb, cgi,sys,json
+cgitb.enable()
+
+data = sys.stdin.read()
+
+#
+print "Content-Type: text/html;charset=utf-8"
+print
+
+request = json.loads(data)
+
+file = request["file"]
+user = request["userUid"]
+
+print "Processing this file: " + file
+print "For user: " + user
